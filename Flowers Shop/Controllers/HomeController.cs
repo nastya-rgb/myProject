4d99ac1e26b58e22888aa1a693aa1f30a3038bee
qsandrew <sanches_andrew@mail.ru>
2020-03-07 14:38:34 +0600

    Add project files.

---
+++ Flowers Shop/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Flowers_Shop.Models;
+
+namespace Flowers_Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Way()
+        {
+            return View();
+        }
+        public IActionResult Music()
+        {
+            return View();
+        }
+       
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Muse()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
